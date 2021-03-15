fe6dd546f630fe90c8df51c62af0fe3a7bdf167a
pamodaaw <pamodaaw@gmail.com>
2021-03-16 02:49:12 +0530

    Load the common guide as a component

---
+++ docs/.vuepress/components/CommonGuide.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+   <Content :pageKey="getPageKey()" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "CommonGuide",
+  props: [ 'guide' ],
+  methods: {
+      getPageKey() {
+        const pages = this.$site.pages
+        var requiredGuideKey;
+        for (var i in pages) {
+        	if(pages[i].relativePath == this.guide){
+        	  requiredGuideKey = pages[i].key;
+        	  }
+        	}
+        return requiredGuideKey;
+        }
+    }
+};
+</script>
