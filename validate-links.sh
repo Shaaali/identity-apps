e85f851039ad89304e260f4b49b436bc5814b477
Farasath Ahamed <farasatha@wso2.com>
2021-06-14 23:01:50 +0530

    Add a script to validate links in the doc HTML site locally
    Part of solution for https://github.com/wso2-enterprise/asgardeo-docs/issues/240

---
+++ validate-links.sh
@@ -0,0 +1,25 @@
+### Detects any broken links in the generated HTML site
+### Script based on the idea in https://matthewsetter.com/writing-tools/npm-broken-link-checker/
+### Broken Link Checker NPM library: https://github.com/stevenvachon/broken-link-checker
+
+# For the broken link checker to work, we need to serve the docs locally. We are using npm serve command for this
+PORT=4567
+
+# Kill any running serves we used to host the docs
+lsof -ti:$PORT | xargs kill
+
+npm run build
+cd docs/.vuepress/dist/ || exit
+
+# https://www.npmjs.com/package/serve
+serve -p $PORT &
+sleep 5
+
+# https://www.npmjs.com/package/broken-link-checker
+if blc --input http://localhost:$PORT -grf --exclude /assets; then
+  echo "No broken links found."
+  exit 0
+else
+  echo "Broken links found."
+  exit 1
+fi
