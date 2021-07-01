197b9213f62e2d7e4522d30f67a449448a4fa4ac
Omal <omal@omal-temp.mshome.net>
2021-07-01 10:21:49 +0530

    Add search and parameterized component in quickstarts index page

---
+++ docs/.vuepress/components/QuickstartItemCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tech-item" @click="navigate(qsPath)">
+    <a>
+      <img
+        class="tech-icon"
+        :src="require(`../theme/assets/${ techIconPath }`)"
+        width="100"
+        height="100"
+      />
+    </a>
+    <p class="tech-name" v-text="techName"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuickstartItemCard",
+  methods: {
+    navigate(url) {
+      void this.$router.push(url);
+    },
+  },
+  props: {
+    techIconPath: String,
+    techName: String,
+    qsPath: String
+  },
+};
+</script>
+
+<style src="../theme/styles/components/quickstartOverview.styl" lang="stylus"/>
