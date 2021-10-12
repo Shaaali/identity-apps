affaef80153594e738a432bcd9df7037921eb758
DonOmalVindula <omalvindula@live.com>
2021-10-12 11:33:58 +0530

    Update google analytics script

--- docs/.vuepress/public/scripts/analytics.js
+++ docs/.vuepress/public/scripts/analytics.js
@@ -9,5 +9,5 @@
     a.src = g;
     m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-ga('create', '', 'auto');
+ga('create', '{ANALYTICS_TAG_ID}', 'auto');
 ga('send', 'pageview');
