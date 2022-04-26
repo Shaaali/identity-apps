c8cd8a1efa78a39e14f86f3c08f0ec9f3e012d64
SujanSanjula96 <sanjulasujan@gmail.com>
2022-04-26 12:23:19 +0530

    Fix minor issue with shellcheck

--- check-links.sh
+++ check-links.sh
@@ -24,7 +24,8 @@ sleep 5
 USER_AGENT=""
 
 # https://www.npmjs.com/package/broken-link-checker
-if blc --input http://localhost:$PORT/asgardeo/docs/  --user-agent="$USER_AGENT" -grf; then
+# shellcheck disable=SC2086
+if blc --input http://localhost:$PORT/asgardeo/docs/  --user-agent=$USER_AGENT -grf; then
   echo "No broken links found."
   exit 0
 else
